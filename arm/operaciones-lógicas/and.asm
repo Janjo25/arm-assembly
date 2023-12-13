f84e2707820c5ba217f187bdfc041e7dbaebacf2
Luis César Lizama Quiñónez <ElLuisLizama25@gmail.com>
2023-12-12 20:34:59 -0600

    refactor: reorganize "and.asm" code

--- arm/operaciones-lógicas/and.asm
+++ arm/operaciones-lógicas/and.asm
@@ -3,10 +3,10 @@
 .section .text
 
 _start:
-    mov R7, #0x01
     mov R1, #25
     AND R0, R1, #1  // Operación "AND" bit a bit entre "R1" y "#1". El "LSB" determina la paridad (R0=1 si impar, R0=0 si par).
 
+    mov R7, #0x01
     swi 0
 
 .section .data
