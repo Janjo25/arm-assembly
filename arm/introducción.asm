41a6575d50d2e4e701653b71c2cfd5fb14a6af3a
Luis César Lizama Quiñónez <ElLuisLizama25@gmail.com>
2023-12-22 11:40:04 -0600

    refactor: change entry point and system call convention

--- arm/introducción.asm
+++ arm/introducción.asm
@@ -1,11 +1,10 @@
-.global _start      // Declara "_start" como un símbolo global. Este el punto de entrada del programa.
+.global main        // Declara "main" como un símbolo global. Este el punto de entrada del programa.
 
 .section .text      // La sección ".text" señala que, a partir de este punto hacia abajo, se encuentra el código del programa.
 
-_start:             // Etiqueta que indica el comienzo del programa.
-    mov r7, #0x01   // Se configura "r7" para indicar que, al realizar "swi", se ejecutará la acción correspondiente al código asignado a "r7".
-    mov r0, #25     // Carga el código de salida "25" en "r0".
-
-    swi 0           // Realiza una llamada al sistema. El número 0 puede ser por convención y generalmente se ignora, pero "r7" determina la acción.
+main:               // Etiqueta que indica el comienzo del programa.
+    MOV r7, #0x01   // Se configura "R7" para indicar que, al realizar "SVC", se ejecutará la acción correspondiente al código asignado a "R7".
+    MOV r0, #25     // Carga el código de salida "25" en "R0".
+    SVC 0           // Realiza una llamada al sistema. El número 0 puede ser por convención y generalmente se ignora, pero "R7" determina la acción.
 
 .section .data      // Inicia la sección ".data", típicamente utilizada para datos inicializados.
