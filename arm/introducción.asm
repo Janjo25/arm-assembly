0251c9bcdb6ab0f8f3de5f44690678023a8fc7e0
Luis César Lizama Quiñónez <ElLuisLizama25@gmail.com>
2023-12-22 11:45:19 -0600

    style: standardize register notation to uppercase

--- arm/introducción.asm
+++ arm/introducción.asm
@@ -3,8 +3,8 @@
 .section .text      // La sección ".text" señala que, a partir de este punto hacia abajo, se encuentra el código del programa.
 
 main:               // Etiqueta que indica el comienzo del programa.
-    MOV r7, #0x01   // Se configura "R7" para indicar que, al realizar "SVC", se ejecutará la acción correspondiente al código asignado a "R7".
-    MOV r0, #25     // Carga el código de salida "25" en "R0".
+    MOV R7, #0x01   // Se configura "R7" para indicar que, al realizar "SVC", se ejecutará la acción correspondiente al código asignado a "R7".
+    MOV R0, #25     // Carga el código de salida "25" en "R0".
     SVC 0           // Realiza una llamada al sistema. El número 0 puede ser por convención y generalmente se ignora, pero "R7" determina la acción.
 
 .section .data      // Inicia la sección ".data", típicamente utilizada para datos inicializados.
